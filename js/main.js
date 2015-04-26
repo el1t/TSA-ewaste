79ce831b1cd308bdbe0e17a874054a157cc175ba
Ellis Tsung <el1t@ymail.com>
2015-04-26 00:25:15 -0400

    Added interpolation
    And a chart library :)
    This index.html is gonna be huge.

--- js/main.js
+++ js/main.js
@@ -15,8 +15,53 @@ $(function() {
 		scrollTop = $window.scrollTop();
 		var scrollPercent = scrollTop / (windowHeight * 1.5);
 		if(scrollPercent >= 0) {
-			sliderZoom.progress(scrollPercent).pause();
+			TweenLite.to(sliderZoom.pause(), 0.1, {time: scrollPercent});
 		}
 		console.log(sliderZoom.progress());
+	});
+});
+
+$(function () {
+	// Create the chart
+	$('#container').highcharts({
+		chart: {
+			type: 'pie'
+		},
+		title: {
+			text: 'Some chart!'
+		},
+		yAxis: {
+			title: {
+				text: 'What a nice animation'
+			}
+		},
+		plotOptions: {
+			pie: {
+				shadow: false,
+				center: ['50%', '50%']
+			}
+		},
+		tooltip: {
+			valueSuffix: '%'
+		},
+		series: [{
+			type: 'pie',
+			name: 'Data',
+			innerSize: '50%',
+			data: [
+				['A', 45.0],
+				['B', 26],
+				['C', 13.6],
+				['D', 8.5],
+				['E', 6.2],
+				{
+					name: 'F',
+					y: 0.7,
+					dataLabels: {
+						enabled: false
+					}
+				}
+			]
+		}]
 	});
 });
