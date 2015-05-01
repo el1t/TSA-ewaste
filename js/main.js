8e00c3d8e0235a4e0d7ed4355e0732062fa7a35a
Ellis Tsung <el1t@ymail.com>
2015-05-01 03:27:53 -0400

    Add hexagon grid
    Of chems

--- js/main.js
+++ js/main.js
@@ -260,7 +260,7 @@ $(document).ready(function() {
 		triggerHook: "onLeave",
 		offset: windowHeight / 2
 	}).setTween(new TimelineLite()
-		.fromTo("#pirate", 0.5, {css: {zoom: 2, autoAlpha: 0}}, {css: {zoom: 1, autoAlpha: 1}})
+		.fromTo("#pirate", 0.75, {css: {zoom: 2, autoAlpha: 0}}, {css: {zoom: 1, autoAlpha: 1}})
 		.fromTo($("#pirate div"), 0.5, {css: {display: "block", autoAlpha: 0}}, {css: {autoAlpha: 1}})
 	).addTo(controller);
 });
