63bba4770451573dad4bd95c03f7266d3c9cfa64
Ellis Tsung <el1t@ymail.com>
2015-05-01 04:42:24 -0400

    Add source view button
    DO NOT UPDATE SCSS

--- js/main.js
+++ js/main.js
@@ -280,4 +280,16 @@ $(document).ready(function() {
 		.fromTo("#pirate", 0.75, {css: {zoom: 2, autoAlpha: 0}}, {css: {zoom: 1, autoAlpha: 1}})
 		.fromTo($("#pirate div"), 0.5, {css: {display: "block", autoAlpha: 0}}, {css: {autoAlpha: 1}})
 	).addTo(controller);
+
+	var $sources = $("#sources"),
+		$sourceContent = $sources.children(".content");
+	$("footer").click(function() {
+		$sources.addClass("active");
+	});
+	$sources.click(function() {
+		$sources.removeClass("active");
+	});
+	$sourceContent.click(function(event) {
+		event.stopPropagation();
+	});
 });
