b6ac9492f1405266a13f1b68ccdc1a9e0a6b5032
Ellis Tsung <el1t@ymail.com>
2015-05-29 11:06:01 -0400

    Add hero to solution

--- js/solution.js
+++ js/solution.js
@@ -2,4 +2,7 @@ $(function() {
 	var windowHeight = $.viewportH(),
 		windowWidth = $.viewportW(),
 		controller = new ScrollMagic.Controller();
+	$("footer").click(function() {
+		window.location.href = "./";
+	});
 });
