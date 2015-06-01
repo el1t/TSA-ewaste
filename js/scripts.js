2732ce57e6f2b6064006d10580685470a5c2fd92
Ellis Tsung <el1t@ymail.com>
2015-05-31 20:21:10 -0400

    Make statusbar responsive to resize
    Add and remove menu when resizing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(function() {
-	var smallScreen = window.matchMedia("only screen and (max-width: 480px)").matches;
-	// Setup menu animation
+	// Add statusbar for standalone version
 	if (window.navigator.standalone) {
 		$("header").addClass("webapp");
 		$("main").addClass("webapp");
@@ -12,28 +11,40 @@ $(function() {
 			location.href = $(e.target).attr("href");
 		});
 	}
-	if (smallScreen) {
-		$("#title").after("<i class=\"fa fa-chevron-circle-down\" id=\"menu-toggle\" ontouchstart=\"\"></i>");
-		var $menu = $("#menu"),
+	// Setup menu animation
+	var recalculate = function() {
+		var smallScreen = window.matchMedia("only screen and (max-width: 480px)").matches,
 			$toggle = $("#menu-toggle");
-			$main = $("main");
-		$toggle.click(function() {
-			$main.toggleClass("blur");
-			$toggle.toggleClass("active");
-			$menu.toggleClass("active");
-		});
-		$main.on("touchstart", function(e) {
-			if ($(this).hasClass("blur")) {
-				e.stopPropagation();
-				$main.removeClass("blur");
-				$toggle.removeClass("active");
-				$menu.removeClass("active");
+		if (smallScreen) {
+			if ($toggle.length) {
+				return;
 			}
-		});
-		$("header").on("touchmove", function(e) {
-			if ($menu.hasClass("active")) {
-				e.preventDefault();
-			}
-		});
-	}
+			var $menu = $("#menu"),
+				$main = $("main");
+			$toggle = $("<i class=\"fa fa-chevron-circle-down\" id=\"menu-toggle\" ontouchstart=\"\"></i>")
+				.insertAfter($("#title"));
+			$toggle.click(function () {
+				$main.toggleClass("blur");
+				$toggle.toggleClass("active");
+				$menu.toggleClass("active");
+			});
+			$main.on("touchstart", function (e) {
+				if ($(this).hasClass("blur")) {
+					e.stopPropagation();
+					$main.removeClass("blur");
+					$toggle.removeClass("active");
+					$menu.removeClass("active");
+				}
+			});
+			$("header").on("touchmove", function (e) {
+				if ($menu.hasClass("active")) {
+					e.preventDefault();
+				}
+			});
+		} else {
+			$toggle.remove();
+		}
+	};
+	$(window).resize(recalculate);
+	recalculate();
 });
